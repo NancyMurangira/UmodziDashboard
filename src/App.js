8946be2e4ac2d78553a17815b509fe979f1b2562
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-07-20 16:37:25 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Settings from './Settings';
-// import Sidebar from './Sidebar';
+
 
 function App() {
   return (
     <div>
      <Settings/>
-     {/* <Sidebar/> */}
     </div>
   );
 }
